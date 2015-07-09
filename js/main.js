814f42121cc12bc777fc2104a60dbf5bcaea50ee
zibs <elizibin@gmail.com>
2015-07-09 10:27:45 -0700

    post forage

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ $(document).ready(function(){
     $(".titlewhite").delay(0).fadeIn(500)
 });
 
-
 $(document).ready(function () {
    $("#query").hover(function () {
     $(".homepage, .navbar").fadeTo("fast",0.08)
@@ -22,6 +21,64 @@ $(document).ready(function () {
 	});
   });
 
+
+// SET LEFT-TRILATERALONE ON PAGE LOAD AND CLEAR
+$(document).ready(function () {
+    setTimeout(function() { 
+  var height_of_element = $(".forages").height();  
+  $(".iv-one-l").css({"width": (height_of_element + "px")});
+  // $(".clear").css({"clear": "both"});
+  }, 500)
+});
+
+// SET RIGHT-TRILATERALONE ON PAGE LOAD AND CLEAR
+$(document).ready(function () {
+    setTimeout(function() { 
+  var height_of_element = $(".forages").height();  
+  $(".iv-one-r").css({"width": (height_of_element + "px")});
+  }, 500)
+});
+
+// RESET LEFT LATERAL ON RESIZE AND CLEAR
+$(document).ready(function () {
+  $(window).resize(function () {
+  var height_of_element = $(".forages").height();  
+  $(".left-lateral").css({"width": (height_of_element + "px")});
+  });
+});
+
+// RESET RIGHT LATERAL ON RESIZE AND CLEAR
+$(document).ready(function () {
+  $(window).resize(function () {
+  var height_of_element = $(".forages").height();  
+  $(".right-lateral").css({"width": (height_of_element + "px")});
+  });
+});
+
+
+
+  
+
+
+  // SET LEFT-TRILATERALTWO ON PAGE LOAD AND CLEAR
+// $(document).ready(function () {
+    // setTimeout(function() { 
+  // /var height_of_element = $(".trilateraltwo").height();  
+  // $(".iv-two-l").css({"width": (height_of_element + "px")});
+  // $(".clear").css({"clear": "both"});
+  // }, 1000)
+// });
+
+// SET RIGHT-TRILATERALTWO ON PAGE LOAD AND CLEAR
+// $(document).ready(function () {
+    // setTimeout(function() { 
+  // var height_of_element = $(".trilateraltwo").height();  
+  // $(".iv-two-r").css({"width": (height_of_element + "px")});
+  // $(".clear").css({"clear": "both"});
+  // }, 500)
+// });
+  // $(".right-lateral").css("width", (height_of_element + "px"));
+// });
 
 
 // Nav adjust on scroll eventually
